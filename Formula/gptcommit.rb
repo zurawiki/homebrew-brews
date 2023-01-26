19558a3bc09094feace646ab72fabdd2b582f534
Roger Zurawicki <zurawiki@users.noreply.github.com>
2023-01-26 14:30:49 -0500

    Add GPTcommit formula & update README

---
+++ Formula/gptcommit.rb
@@ -0,0 +1,20 @@
+class Gptcommit < Formula
+  desc "A git prepare-commit-msg hook for authoring commit messages with GPT-3."
+  homepage "https://github.com/zurawiki/gptcommit"
+  url "https://github.com/zurawiki/gptcommit/archive/v0.1.10.tar.gz"
+  sha256 "f3eae89fe66b1c6e6100bb01a82031c524da8fc40243e7f8914876994b932df7"
+  head "https://github.com/zurawiki/gptcommit.git", branch: "main"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  uses_from_macos "zlib"
+
+  def install
+   system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "0.1.10", shell_output("#{bin}/gptcommit -V")
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# homebrew-gptcommit
+# `gptcommit`
+
+This is the `brew` formula for [`gptcommit`](https://github.com/zurawiki/gptcommit).
+
+Check out the blog post [here](https://zura.wiki/post/never-write-a-commit-message-again-with-the-help-of-gpt-3/).
+
+## How do I install these formulae?
+
+`brew install zurawiki/gptcommit/<formula>`
+
+e.g. `brew install zurawiki/gptcommit/gptcommit`
+
+
+Or `brew tap zurawiki/gptcommit` and then `brew install gptcommit`.
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
