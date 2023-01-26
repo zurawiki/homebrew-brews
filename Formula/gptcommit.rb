882805eba490424b03ffbc7d1264be5625cc05ff
Roger Zurawicki <zurawiki@users.noreply.github.com>
2023-01-26 15:36:12 -0500

    Update to v0.1.11

--- Formula/gptcommit.rb
+++ Formula/gptcommit.rb
@@ -1,7 +1,7 @@
 class Gptcommit < Formula
   desc "Git prepare-commit-msg hook for authoring commit messages with GPT-3"
   homepage "https://github.com/zurawiki/gptcommit"
-  url "https://github.com/zurawiki/gptcommit/archive/v0.1.10.tar.gz"
+  url "https://github.com/zurawiki/gptcommit/archive/v0.1.11.tar.gz"
   sha256 "f3eae89fe66b1c6e6100bb01a82031c524da8fc40243e7f8914876994b932df7"
   license "MIT"
   head "https://github.com/zurawiki/gptcommit.git", branch: "main"
@@ -15,6 +15,6 @@ class Gptcommit < Formula
   end
 
   test do
-    assert_match "0.1.10", shell_output("#{bin}/gptcommit -V")
+    assert_match "0.1.11", shell_output("#{bin}/gptcommit -V")
   end
 end
