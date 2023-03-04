b36b651441103447747dc1eb9b7346a90875edc2
Roger Zurawicki <zurawiki@users.noreply.github.com>
2023-03-04 10:20:28 -0500

    Update version to 0.3.0

--- Formula/gptcommit.rb
+++ Formula/gptcommit.rb
@@ -1,9 +1,9 @@
 class Gptcommit < Formula
-  version = "0.1.13"
+  version = "0.3.0"
   desc "Git prepare-commit-msg hook for authoring commit messages with GPT-3"
   homepage "https://github.com/zurawiki/gptcommit"
   url "https://github.com/zurawiki/gptcommit/archive/v" + version.to_s + ".tar.gz"
-  sha256 "e83317e2f52b56548f5b5f10274b3dc9434a3952bb2d0eb0bee56a745d939236"
+  sha256 "cdab6cee90e159c405e075c9ac80f89c613b8ffa85f11625f936cb1c93b24f67"
   license "MIT"
   head "https://github.com/zurawiki/gptcommit.git", branch: "main"
 
--- README.md
+++ README.md
@@ -14,3 +14,15 @@ Or `brew tap zurawiki/brews` and then `brew install gptcommit`.
 ## Documentation
 
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+
+## Updating these formula
+
+1. Bump the version in the corresponding `*.rb` file
+
+2. Install the new bottle
+```sh
+brew install --build-bottle Formula/gptcommit.rb
+```
+
+3. Update any checksums as neccesary. Make sure the checksums are verified manually.
